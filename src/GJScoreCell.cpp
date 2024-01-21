9d00886994c09d1da8ebf510dd3befda71882cc0
Cvolton <cvolton@cvolton.eu>
2024-01-21 01:00:44 +0100

    add bottom layout to GJScoreCell

--- src/GJScoreCell.cpp
+++ src/GJScoreCell.cpp
@@ -46,6 +46,50 @@ $register_ids(GJScoreCell) {
             static_cast<CCNode*>(menu->getChildren()->objectAtIndex(0))->setID("player-name");
         }
     }
+
+    std::array<const char*, 10> nodes = {
+        "diamonds-label",
+        "diamonds-icon",
+        "coins-label",
+        "coins-icon",
+        "user-coins-label",
+        "user-coins-icon",
+        "demons-label",
+        "demons-icon",
+        "creator-points-label",
+        "creator-points-icon"
+    };
+
+    auto statsMenu = CCMenu::create();
+    statsMenu->setLayout(
+        RowLayout::create()
+            ->setGap(3.f)
+            ->setAxisAlignment(AxisAlignment::Start)
+    );
+    statsMenu->setID("stats-menu");
+    statsMenu->setPosition({(m_width / 2) + 29, (m_height / 2) - 13});
+    statsMenu->setContentSize({290, 90});
+    statsMenu->setZOrder(10);
+    m_mainLayer->addChild(statsMenu);
+
+    size_t idx = 0;
+    for(auto node: nodes) {
+        if(auto child = m_mainLayer->getChildByID(node)) {
+            auto options = AxisLayoutOptions::create()
+                ->setMinScale(.1f)
+                ->setRelativeScale(child->getScale()
+            );
+            if(idx++ % 2 == 1) options->setNextGap(7.f);
+
+
+            child->removeFromParent();
+            child->setLayoutOptions(options);
+            statsMenu->addChild(child);
+        }
+    }
+
+    statsMenu->updateLayout();
+    
 }
 
 struct GJScoreCellIDs : Modify<GJScoreCellIDs, GJScoreCell> {
