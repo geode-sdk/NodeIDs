1614aff29881d3e6aae348e3b02a8a6beb2f5346
altalk23 <45172705+altalk23@users.noreply.github.com>
2024-02-04 22:19:00 +0300

    #help #geode #update #beta #14

--- src/main.cpp
+++ src/main.cpp
@@ -2,3 +2,15 @@
 #include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
+
+void helpGeodeUpdate() {
+    auto* mod = Loader::get()->getLoadedMod("geode.loader");
+    auto value = mod->getSavedValue<std::string>("last-modified-auto-update-check");
+    if (value.ends_with('\r')) {
+        mod->setSavedValue("last-modified-auto-update-check", value.substr(0, value.size() - 1));
+    }
+}
+
+$on_mod(Loaded) {
+    helpGeodeUpdate();
+}
