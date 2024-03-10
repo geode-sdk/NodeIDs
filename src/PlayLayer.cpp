6033867596feff9309a09768e7fe27a51b60d83a
Terma <termafaxplay@outlook.com>
2024-03-10 02:55:46 -0300

    Revert change in PlayLayer

--- src/PlayLayer.cpp
+++ src/PlayLayer.cpp
@@ -10,7 +10,7 @@ using namespace geode::node_ids;
 
 $register_ids(PlayLayer) {
     setIDSafe(this, 1, "main-node");
-    setIDSafe<CCSprite>(static_cast<CCNode*>(this->getChildren()->objectAtIndex(0)), 0, "background");
+    setIDSafe<CCSprite>(static_cast<CCNode*>(this->getChildren()->objectAtIndex(1)), 0, "background");
 
     setIDSafe(this, 3, "hitbox-node");
     setIDSafe<GJEffectManager>(this, 0, "effect-manager");
