a54b011b0f67df2b85e6610ef6fc4b101928fae7
masp005 <73901337+MaSp005@users.noreply.github.com>
2024-03-10 16:03:29 +0100

    Implement SecretRewardsLayer (chest room) (#28)
    * Implement SecretRewardsLayer (chest room)
    
    * dammit i messed up the names

---
+++ src/SecretRewardsLayer.cpp
@@ -0,0 +1,142 @@
+#include <Geode/Bindings.hpp>
+#include <Geode/modify/SecretRewardsLayer.hpp>
+#include <Geode/utils/cocos.hpp>
+#include <Geode/utils/NodeIDs.hpp>
+
+using namespace geode::prelude;
+using namespace geode::node_ids;
+
+// TODO: Add IDs to individual chest rooms
+
+void assignIDsToStore(CCMenuItemSpriteExtra* store, int storeNum) {
+  switch (storeNum) {
+    case 0:
+      store->setID("scratch-shop");
+      break;
+    case 1:
+      store->setID("creator-shop");
+      break;
+    case 2:
+      store->setID("mechanic-shop"); // suffix "-shop" merely for consistency
+      break;
+    case 3:
+      store->setID("diamond-shop");
+      break;
+  }
+
+  auto sprite = getChildOfType<CCSprite>(store, 0);
+  sprite->setID("store-sprite");
+
+  // CCNode *node = NULL;
+  // CCARRAY_FOREACH(sprite->getChildren(), node)
+  // {
+  //   node->setID("rope");
+  // }
+}
+
+void assignIDsToPage(CCLayer* page, int pageNum) {
+  page->setID("page" + std::to_string(pageNum + 1));
+
+  if (pageNum == 3) { // store page
+    if (auto menu = getChildOfType<CCMenu>(page, 0)) {
+      menu->setID("store-menu");
+
+      // make this a row layout in case people want to add their own shops here
+      menu->setPosition(285.f, 220.f);
+      menu->setLayout(
+        RowLayout::create()
+          ->setGap(25.f)
+      );
+      menu->updateLayout();
+
+      assignIDsToStore(getChildOfType<CCMenuItemSpriteExtra>(menu, 0), 0);
+      assignIDsToStore(getChildOfType<CCMenuItemSpriteExtra>(menu, 1), 1);
+      assignIDsToStore(getChildOfType<CCMenuItemSpriteExtra>(menu, 2), 2);
+      assignIDsToStore(getChildOfType<CCMenuItemSpriteExtra>(menu, 3), 3);
+    }
+    return;
+  }
+
+  if (auto menu = getChildOfType<CCMenu>(page, 0)) {
+    menu->setID("chests-menu");
+
+    setIDSafe<CCMenuItemSpriteExtra>(menu, 0, ("chest" + std::to_string(pageNum * 3 + 1)).c_str());
+    setIDSafe<CCMenuItemSpriteExtra>(menu, 1, ("chest" + std::to_string(pageNum * 3 + 2)).c_str());
+    setIDSafe<CCMenuItemSpriteExtra>(menu, 2, ("chest" + std::to_string(pageNum * 3 + 3)).c_str());
+  }
+
+  setIDSafe<CCLabelBMFont>(page, 0, ((pageNum == 2 ? "chest" : "chest" + std::to_string(pageNum * 3 + 1)) + "-counter").c_str()); // on all pages
+  if (pageNum < 2) { // only on page 0 & 1
+    setIDSafe<CCLabelBMFont>(page, 1, ((pageNum == 2 ? "milestone100" : "chest" + std::to_string(pageNum * 3 + 2)) + "-counter").c_str());
+    setIDSafe<CCLabelBMFont>(page, 2, ((pageNum == 2 ? "milestone200" : "chest" + std::to_string(pageNum * 3 + 3)) + "-counter").c_str());
+  }
+
+  // don't mind the first three indeces down there, for some reason the third page mixes them up idk
+  // also these calls are pretty repetitive so they are all in 1 line bc wrapping them looks frankly disgusting lol
+  setIDSafe<CCSprite>(page, pageNum == 2 ? 2 : 0, pageNum == 2 ? "chest-icon" : "tier-indicator");
+  setIDSafe<CCSprite>(page, pageNum == 2 ? 0 : 1, ((pageNum == 2 ? "milestone50"  : "chest" + std::to_string(pageNum * 3 + 1)) + "-platform").c_str());
+  setIDSafe<CCSprite>(page, pageNum == 2 ? 1 : 2, ((pageNum == 2 ? "milestone50"  : "chest" + std::to_string(pageNum * 3 + 1)) + "-cost"    ).c_str());
+  setIDSafe<CCSprite>(page, 3,                    ((pageNum == 2 ? "milestone100" : "chest" + std::to_string(pageNum * 3 + 2)) + "-platform").c_str());
+  setIDSafe<CCSprite>(page, 4,                    ((pageNum == 2 ? "milestone100" : "chest" + std::to_string(pageNum * 3 + 2)) + "-cost"    ).c_str());
+  setIDSafe<CCSprite>(page, 5,                    ((pageNum == 2 ? "milestone200" : "chest" + std::to_string(pageNum * 3 + 3)) + "-platform").c_str());
+  setIDSafe<CCSprite>(page, 6,                    ((pageNum == 2 ? "milestone200" : "chest" + std::to_string(pageNum * 3 + 3)) + "-cost"    ).c_str());
+}
+
+$register_ids(SecretRewardsLayer) {    
+    if (auto menu = getChildOfType<CCLayer>(this, 0)) {
+      menu->setID("main-contents");
+
+      if (auto scrollLayer = getChildOfType<CCLayer>(menu, 0)) {
+        scrollLayer->setID("scroll-layer");
+        
+        if (auto pages = getChildOfType<ExtendedLayer>(scrollLayer, 0)) {
+          pages->setID("pages");
+          setIDSafe<cocos2d::CCSprite>(menu, 0, "title-label");
+
+          assignIDsToPage(getChildOfType<CCLayer>(pages, 0), 0);
+          assignIDsToPage(getChildOfType<CCLayer>(pages, 1), 1);
+          assignIDsToPage(getChildOfType<CCLayer>(pages, 2), 2);
+          assignIDsToPage(getChildOfType<CCLayer>(pages, 3), 3);
+        }
+        
+        if (auto pageCircleWrapper = getChildOfType<cocos2d::CCSpriteBatchNode>(scrollLayer, 0)) {
+          pageCircleWrapper->setID("page-circles");
+
+          setIDSafe<CCSprite>(pageCircleWrapper, 0, "page1");
+          setIDSafe<CCSprite>(pageCircleWrapper, 1, "page2");
+          setIDSafe<CCSprite>(pageCircleWrapper, 2, "page3");
+          setIDSafe<CCSprite>(pageCircleWrapper, 3, "page4");
+        }
+      }
+    }
+
+    if (auto pageNav = getChildOfType<CCMenu>(this, 1)) {
+      pageNav->setID("page-navigation");
+
+      setIDSafe<CCMenuItemSpriteExtra>(pageNav, 0, "left");
+      setIDSafe<CCMenuItemSpriteExtra>(pageNav, 1, "right");
+    }
+
+    setIDSafe<CCSprite>(this, 0, "background");
+    setIDSafe<CCSprite>(this, 1, "top-right-art");
+    setIDSafe<CCSprite>(this, 2, "top-left-art");
+    setIDSafe<CCSprite>(this, 3, "key-icon");
+    setIDSafe<CCSprite>(this, 4, "floor");
+    setIDSafe<CCLabelBMFont>(this, 0, "key-count");
+    setIDSafe<CCMenu>(this, 0, "exit-menu");
+};
+
+struct SecretRewardsLayerIDs : Modify<SecretRewardsLayerIDs, SecretRewardsLayer> {
+  static void onModify(auto &self) {
+    if (!self.setHookPriority("SecretRewardsLayer::init", GEODE_ID_PRIORITY)) {
+      log::warn("Failed to set SecretRewardsLayer::init hook priority, node IDs may "
+                "not work properly");
+    }
+  }
+  bool init(bool p0) {
+    if (!SecretRewardsLayer::init(p0))
+      return false;
+    NodeIDs::get()->provide(this);
+    return true;
+  }
+};
