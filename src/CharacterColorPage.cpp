e39b215ec54f8060406c1fa9d4d191e60dfaed42
Weebifying <70783792+Weebifying@users.noreply.github.com>
2024-03-10 07:20:46 -0700

    charactercolorpage :D (#27)
    * charactercolorpage :D
    
    * no bindings for macos yet :(

---
+++ src/CharacterColorPage.cpp
@@ -0,0 +1,73 @@
+#include <Geode/Geode.hpp>
+#ifndef GEODE_IS_MACOS
+
+#include <Geode/Bindings.hpp>
+#include <Geode/modify/CharacterColorPage.hpp>
+#include <Geode/utils/cocos.hpp>
+#include <Geode/utils/NodeIDs.hpp>
+
+using namespace geode::prelude;
+using namespace geode::node_ids;
+
+$register_ids(CharacterColorPage) {
+    size_t offset = 0;
+
+    auto colorsLayer = static_cast<CCLayer*>(this->getChildren()->objectAtIndex(0));
+    colorsLayer->setID("colors-layer");
+
+    setIDSafe(colorsLayer, offset++, "background");
+    if (auto menu = getChildOfType<CCMenu>(colorsLayer, 0)) {
+        menu->setID("buttons-menu");
+        setIDs(
+            menu,
+            0,
+            "close-button",
+            "ship-button",
+            "col1-button",
+            "col2-button",
+            "glow-button"
+        );
+
+        for (int i = 5; i < menu->getChildrenCount()-1; i++) {
+            auto child = as<CCMenuItemSpriteExtra*>(menu->getChildren()->objectAtIndex(i));
+            child->setID(std::to_string(child->getTag()));
+        }
+
+        setIDSafe(menu, menu->getChildrenCount()-1, "glow-toggler");
+    }
+
+    offset++;
+    setIDs(
+        colorsLayer,
+        offset,
+        "icon-background",
+        "cube-icon",
+        "ball-icon",
+        "ufo-icon",
+        "wave-icon",
+        "robot-icon",
+        "spider-icon",
+        "swing-icon",
+        "cursor-col1",
+        "cursor-col2",
+        "cursor-glow",
+        "glow-label"
+    );
+}
+
+struct CharacterColorPageIDS : Modify<CharacterColorPageIDS, CharacterColorPage> {
+    static void onModify(auto& self) {
+        if (!self.setHookPriority("CharacterColorPage::init", GEODE_ID_PRIORITY)) {
+            log::warn("Failed to set CharacterColorPage::init hook priority, node IDs may not work properly");
+        }
+    }
+
+    bool init() {
+        if (!CharacterColorPage::init()) return false;
+
+        NodeIDs::get()->provide(this);
+
+        return true;
+    }
+};
+#endif
