d49e19b3fa7ebb13cb579ef4049bcf345bfba496
SpaghettDev <37266659+SpaghettDev@users.noreply.github.com>
2024-04-12 22:40:29 +0000

    GauntletNode, GauntletSelectLayer, LevelPage, LevelSelectLayer and MoreOptionsLayer (#33)
    * GauntletSelectLayer, LevelPage, LevelSelectLayer and MoreOptionsLayer
    
    * GauntletNode, missing GauntletSelectLayer nodeIDs and typo fixe
    
    * Fix duplicate node and MOL on android
    
    * Fix epic compiler error
    
    * sorru
    
    * GauntletSelectLayer conflict

---
+++ src/GauntletNode.cpp
@@ -0,0 +1,89 @@
+#include <Geode/Geode.hpp>
+#include <Geode/binding/GauntletNode.hpp>
+#include <Geode/modify/GauntletNode.hpp>
+#include <Geode/utils/NodeIDs.hpp>
+
+using namespace geode::prelude;
+using namespace geode::node_ids;
+
+struct GauntletNodeIDs : Modify<GauntletNodeIDs, GauntletNode> {
+    GJMapPack* m_gauntlet;
+
+    static void onModify(auto& self) {
+        if (!self.setHookPriority("GauntletNode::init", GEODE_ID_PRIORITY)) {
+            log::warn("Failed to set GauntletNode::init hook priority, node IDs may not work properly");
+        }
+    }
+
+    bool init(GJMapPack* gauntlet) {
+        if (!GauntletNode::init(gauntlet)) return false;
+
+        m_fields->m_gauntlet = gauntlet;
+
+        NodeIDs::get()->provide(this);
+
+        return true;
+    }
+};
+
+$register_ids(GauntletNode) {
+    const auto GSM = GameStatsManager::sharedState();
+    auto self = reinterpret_cast<GauntletNodeIDs*>(this);
+
+    if (self->m_fields->m_gauntlet->hasCompletedMapPack()) {
+        setIDs(
+            this,
+            0,
+            "gauntlet-info-node",
+            "empty-node",
+            "background",
+            "gauntlet-name-label",
+            "gauntlet-label",
+            "gauntlet-name-shadow-label",
+            "gauntlet-shadow-label",
+            "checkmark-sprite",
+            "checkmark-shadow-sprite"
+        );
+    } else {
+        setIDs(
+            this,
+            0,
+            "gauntlet-info-node",
+            "empty-node",
+            "background",
+            "gauntlet-name-label",
+            "gauntlet-label",
+            "gauntlet-name-shadow-label",
+            "gauntlet-shadow-label"
+        );
+    }
+
+    if (auto gauntletInfoNode = this->getChildByID("gauntlet-info-node")) {
+        if (
+            self->m_fields->m_gauntlet->hasCompletedMapPack() &&
+            GSM->isSpecialChestUnlocked(GSM->getGauntletRewardKey(self->m_fields->m_gauntlet->m_packID))
+        ) {
+            setIDs(
+                gauntletInfoNode,
+                0,
+                "gauntlet-sprite",
+                "gauntlet-shadow-sprite",
+                "gauntlet-progress-label",
+                "gauntlet-progress-shadow-label"
+            );
+        } else {
+            setIDs(
+                gauntletInfoNode,
+                0,
+                "gauntlet-sprite",
+                "gauntlet-shadow-sprite",
+                "gauntlet-progress-label",
+                "gauntlet-progress-shadow-label",
+                "reward-label",
+                "reward-shadow-label",
+                "chest-sprite",
+                "chest-shadow-sprite"
+            );
+        }
+    }
+}
--- src/GauntletSelectLayer.cpp
+++ src/GauntletSelectLayer.cpp
@@ -1,12 +1,17 @@
-#include <Geode/Bindings.hpp>
+#include <Geode/Geode.hpp>
+#include <Geode/binding/GauntletSelectLayer.hpp>
 #include <Geode/modify/GauntletSelectLayer.hpp>
-#include <Geode/utils/cocos.hpp>
 #include <Geode/utils/NodeIDs.hpp>
 
 using namespace geode::prelude;
 using namespace geode::node_ids;
 
 $register_ids(GauntletSelectLayer) {
+    this->setID("main-layer");
+
+    getChildOfType<CCSprite>(this, 0)->setID("background");
+    getChildOfType<CCSprite>(this, 1)->setID("title-sprite");
+
     setIDs(
         this,
         0,
@@ -21,6 +26,25 @@ $register_ids(GauntletSelectLayer) {
         "loading-circle",
         "try-again-text"
     );
+
+    if (auto arrowsMenu = this->getChildByID("scroll-buttons-menu")) {
+        setIDs(
+            arrowsMenu,
+            0,
+            "left-button",
+            "right-button"
+        );
+    }
+
+    if (auto miscMenu = this->getChildByID("left-menu")) {
+        setIDs(
+            miscMenu,
+            0,
+            "back-button",
+            "info-button",
+            "reload-button"
+        );
+    }
 }
 
 struct GauntletSelectLayerIDs : Modify<GauntletSelectLayerIDs, GauntletSelectLayer> {
@@ -28,13 +52,53 @@ struct GauntletSelectLayerIDs : Modify<GauntletSelectLayerIDs, GauntletSelectLay
         if (!self.setHookPriority("GauntletSelectLayer::init", GEODE_ID_PRIORITY)) {
             log::warn("Failed to set GauntletSelectLayer::init hook priority, node IDs may not work properly");
         }
+        if (!self.setHookPriority("GauntletSelectLayer::setupGauntlets", GEODE_ID_PRIORITY)) {
+            log::warn("Failed to set GauntletSelectLayer::setupGauntlets hook priority, node IDs may not work properly");
+        }
     }
 
-    bool init(int idk) {
-        if (!GauntletSelectLayer::init(idk)) return false;
+    bool init(int p0) {
+        if (!GauntletSelectLayer::init(p0)) return false;
 
         NodeIDs::get()->provide(this);
 
         return true;
+    }
+
+    void setupGauntlets() {
+        GauntletSelectLayer::setupGauntlets();
+
+        if (auto gauntletsList = getChildOfType<BoomScrollLayer>(this, 0)) {
+            gauntletsList->setID("gauntlets-list");
+
+            if (auto gauntletPages = getChildOfType<ExtendedLayer>(gauntletsList, 0)) {
+                gauntletPages->setID("gauntlet-pages");
+
+                auto gauntletPagesArray = CCArrayExt<CCLayer*>(gauntletPages->getChildren());
+                for (int i = 0; i < gauntletPagesArray.size(); i++) {
+                    auto gauntletPage = gauntletPagesArray[i];
+                    gauntletPage->setID(fmt::format("gauntlet-page-{}", i + 1));
+
+                    if (auto gauntletMenu = getChildOfType<CCMenu>(gauntletPage, 0)) {
+                        gauntletMenu->setID("gauntlet-menu");
+
+                        auto gauntletButtons = CCArrayExt<CCMenuItemSpriteExtra*>(gauntletMenu->getChildren());
+                        for (int i = 0; i < gauntletButtons.size(); i++) {
+                            auto gauntletButton = gauntletButtons[i];
+
+                            gauntletButton->setID(fmt::format("gauntlet-button-{}", i + 1));
+
+                            if (auto gauntletNodeParent = getChildOfType<CCNode>(gauntletButton, 0)) {
+                                gauntletNodeParent->setID("gauntlet-node-parent");
+
+                                getChildOfType<GauntletNode>(gauntletNodeParent, 0)->setID("gauntlet-node");
+                            }
+                        }
+                    }
+                }
+            }
+
+            getChildOfType<CCSpriteBatchNode>(gauntletsList, 0)->setID("page-buttons");
+        }
     }
 };
