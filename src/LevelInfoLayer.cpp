a96ad62cd37b996b4e975c4cb68e75caad89fa95
Justin <52604018+hiimjustin000@users.noreply.github.com>
2024-04-23 23:46:31 -0400

    Update LevelInfoLayer.cpp

--- src/LevelInfoLayer.cpp
+++ src/LevelInfoLayer.cpp
@@ -55,6 +55,13 @@ $register_ids(LevelInfoLayer) {
 
     setIDSafe<CCSprite>(this, 8 + iconOffset, "stars-icon");
 
+    if (m_level->m_dailyID > 0 || m_level->m_gauntletLevel) {
+        setIDSafe<CCLabelBMFont>(this, 6 + labelOffset, "diamond-label");
+        setIDSafe<CCSprite>(this, 9 + iconOffset, "stars-icon");
+        labelOffset++;
+        iconOffset++;
+    }
+
     /*if(m_ldmLabel) m_ldmLabel->setID("ldm-label");
     if(m_ldmToggler) m_ldmToggler->setID("ldm-toggler");*/
     if(m_difficultySprite) m_difficultySprite->setID("difficulty-sprite");
